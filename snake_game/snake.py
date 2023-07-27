74b2542a4d28b6be0ffceec9649b1bca54da9aca
Majed <majedulislam7789@gmail.com>
2023-07-27 20:31:10 +0600

    add food

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -12,6 +12,7 @@ class Snake:
     def __init__(self):
         self.segements = []
         self.create_snake()
+        self.head = self.segements[0]
     
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -26,20 +27,20 @@ class Snake:
             new_x = self.segements[seg_num - 1].xcor()
             new_y = self.segements[seg_num - 1].ycor()
             self.segements[seg_num].goto(new_x, new_y)
-        self.segements[0].forward(20)
+        self.head.forward(20)
     
     def up(self):
-        if self.segements[0].heading() != DOWN:
-            self.segements[0].setheading(UP)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
     
     def down(self):
-        if self.segements[0].heading() != UP:
-            self.segements[0].setheading(DOWN)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
     
     def right(self):
-        if self.segements[0].heading() != LEFT:
-            self.segements[0].setheading(RIGHT)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
     
     def left(self):
-        if self.segements[0].heading() != RIGHT:
-            self.segements[0].setheading(LEFT)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
