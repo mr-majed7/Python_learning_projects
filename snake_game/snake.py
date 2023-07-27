1aa1b5119c22f83734086f332c2c5911979a692b
Majed <majedulislam7789@gmail.com>
2023-07-27 19:46:04 +0600

    snake class

---
+++ snake_game/snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+class Snake:
+
+    def __init__(self):
+        self.segements = []
+        self.create_snake()
+    
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segements.append(new_segment)
+    
+    def move(self):
+        for seg_num in range(len(self.segements) - 1, 0, -1):
+            new_x = self.segements[seg_num - 1].xcor()
+            new_y = self.segements[seg_num - 1].ycor()
+            self.segements[seg_num].goto(new_x, new_y)
+        self.segements[0].forward(20)
+        self.segements[0].left(90)
