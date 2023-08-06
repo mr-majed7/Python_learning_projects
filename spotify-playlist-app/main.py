c7dfb75539bf8a8b79b4758457fc2db1e4c3745f
Majed <majedulislam7789@gmail.com>
2023-08-07 00:30:28 +0600

    authenticate spotify account

---
+++ spotify-playlist-app/main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        show_dialog=True,
+        cache_path="token.txt",
+        username="Majedul Islam", 
+    )
+)
+user_id = sp.current_user()["id"]
