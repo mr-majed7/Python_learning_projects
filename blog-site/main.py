558d5416d685cd56f2a200dc65e34a49481d85d8
Majed <majedulislam7789@gmail.com>
2023-08-10 03:05:34 +0600

    add bootstrap theme and styling

--- blog-site/main.py
+++ blog-site/main.py
@@ -5,23 +5,35 @@ import requests
 
 app = Flask(__name__)
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+posts = requests.get("https://api.npoint.io/fccebc96ef732973b267").json()
 post_objects = []
 for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"],post["author"],post["date"],post["image"])
     post_objects.append(post_obj)
 
 @app.route('/')
 def home():
-    return render_template("index.html",blogs=post_objects)
+    return render_template("index.html",posts=post_objects)
 
-@app.route('/getBlog/<id>')
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/getBlog/<int:id>')
 def getBlog(id):
-    for blog in post_objects:
-        if blog.id == int(id):
-            return render_template("post.html",blog=blog)
+    for post in post_objects:
+        if post.id == int(id):
+            return render_template("post.html",post=post)
     return render_template("post.html",blog=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+
+# https://www.npoint.io/docs/fccebc96ef732973b267
