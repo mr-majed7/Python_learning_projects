2f40471fae9263aa880e9b5195eb08499d5c1a24
Majed <majedulislam7789@gmail.com>
2023-08-10 18:55:25 +0600

    add form post route and email sending to blog owner

--- blog-site/main.py
+++ blog-site/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from post import Post
 import requests
+import smtplib
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 
 app = Flask(__name__)
@@ -19,9 +24,13 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact',methods=["GET","POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_mail(data["name"],data["email"],data["phone"],data["message"])
+        return render_template("contact.html",msg_sent=True)
+    return render_template("contact.html",msg_sent=False)
 
 @app.route('/getBlog/<int:id>')
 def getBlog(id):
@@ -29,6 +38,15 @@ def getBlog(id):
         if post.id == int(id):
             return render_template("post.html",post=post)
     return render_template("post.html",blog=None)
+
+def send_mail(name,email,phone,message):
+    my_email = os.getenv("OWNER_EMAIL")
+    password = os.getenv("APP_PASSWORD")
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs=email,msg=f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
