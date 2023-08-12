18aae936fcee59a5369a6663db4f8c7ccaad36dc
Majed <majedulislam7789@gmail.com>
2023-08-12 21:51:30 +0600

    add database

--- blog-site/main.py
+++ blog-site/main.py
@@ -1,29 +1,53 @@
-from flask import Flask, render_template,request
 from post import Post
 import requests
 import smtplib
 import os
 import dotenv
+from flask import Flask, render_template, redirect, url_for,request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 
 dotenv.load_dotenv()
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
 
-posts = requests.get("https://api.npoint.io/fccebc96ef732973b267").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"],post["author"],post["date"],post["image"])
-    post_objects.append(post_obj)
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 def home():
-    return render_template("index.html",posts=post_objects)
+    all_posts = db.session.execute(db.select(BlogPost)).scalars()
+    return render_template("index.html", posts=all_posts)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
-
 @app.route('/contact',methods=["GET","POST"])
 def contact():
     if request.method == "POST":
@@ -34,10 +58,8 @@ def contact():
 
 @app.route('/getBlog/<int:id>')
 def getBlog(id):
-    for post in post_objects:
-        if post.id == int(id):
-            return render_template("post.html",post=post)
-    return render_template("post.html",blog=None)
+    blog = db.session.execute(db.select(BlogPost).where(BlogPost.id == id)).scalar()
+    return render_template("post.html",post=blog)
 
 def send_mail(name,email,phone,message):
     my_email = os.getenv("OWNER_EMAIL")
